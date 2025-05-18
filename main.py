f74e2dfaa732c2a02e04b9d2aee092a1d682ce06
H-M-Nizum <hmnizum1714032@gmail.com>
2025-05-19 02:49:41 +0600

    Extract top 5 frequent words after removing stopwords

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
 
 # Separate and Store messages in appropriate structures for further analysis.
 def parse_chat_log(chat_text):
@@ -32,6 +36,25 @@ def message_statistics(val):
         'total_message' : total_message
     }
 
+# Exclude common stop words
+def clean_and_tokenize(text):
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(text)
+    
+    filtered_tokens = [
+        word for word in tokens
+        if word.lower() not in stop_words and word.isalpha()
+    ]
+    return filtered_tokens
+
+# Extract the top 5 most frequently used words.
+def get_top_keywords(tokens, top_n=5):
+    frequently_word = Counter(tokens)
+    top_most_word = frequently_word.most_common(top_n)
+    top_most_keywords = [word for word, _ in top_most_word]
+    
+    return top_most_keywords
+
 
 # ==== MAIN  ====
 if __name__ == "__main__":
@@ -47,6 +70,11 @@ if __name__ == "__main__":
         message_data = parse_chat_log(chat_content)
         statistics_data = message_statistics(message_data)
         print(statistics_data)
+        
+        combined_text = " ".join(message_data['user_messages'] + message_data['ai_messages'])
+        tokens = clean_and_tokenize(combined_text)
+        top_keywords = get_top_keywords(tokens)
+        print(top_keywords)
         
     else:
         print(f"File '{file_name}' not found in folder '{base_directory}'.")
